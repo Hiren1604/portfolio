9c212028829cb653e4efbda7604f749937a747d5
hiren1604 <waghelahien16@gmail.com>
2024-08-05 11:06:45 +0530

    Main Body and Features Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typingEffect = new Typed(".sec-text", {
+    strings: ["UI/UX Enthusiast", "Developer", "Programmer", "Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+});
+
+
