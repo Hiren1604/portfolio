98d88bb942fa4c85fecffa642835d5cb40208a26
hiren1604 <waghelahien16@gmail.com>
2024-08-07 01:01:18 +0530

    Features Added

--- script.js
+++ script.js
@@ -6,29 +6,6 @@ var typingEffect = new Typed(".sec-text", {
     backDelay: 1520
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-        speed: 3000,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-        },
-        loop: true,
-        slidesPerView: 3, // Adjust this based on your preference
-        spaceBetween: 10,
-        on: {
-            init: function () {
-                this.autoplay.start();
-            },
-            mouseenter: function () {
-                this.autoplay.stop();
-            },
-            mouseleave: function () {
-                this.autoplay.start();
-            }
-        }
-    });
-});
 
 
 
