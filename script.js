42bcdc02d07c7846e4031f1d1671b6d459b7976f
hiren1604 <waghelahien16@gmail.com>
2024-08-06 22:16:51 +0530

    Adding Assets and Code

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 var typingEffect = new Typed(".sec-text", {
-    strings: ["UI/UX Enthusiast", "Developer", "Programmer", "Designer"],
+    strings: ["UI/UX Enthusiast", "Front-End Developer", "Programmer", "Designer"],
     loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1500
+    typeSpeed: 120,
+    backSpeed: 100,
+    backDelay: 1520
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        speed: 3000,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        slidesPerView: 3, // Adjust this based on your preference
+        spaceBetween: 10,
+        on: {
+            init: function () {
+                this.autoplay.start();
+            },
+            mouseenter: function () {
+                this.autoplay.stop();
+            },
+            mouseleave: function () {
+                this.autoplay.start();
+            }
+        }
+    });
+});
+
+
 
